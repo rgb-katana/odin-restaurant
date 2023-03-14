86f3546aeec2b5b506d607573e3f63f4c76e487c
rgb-katana <komarova1610@yandex.ru>
2023-03-14 18:06:37 +0300

    webpack config setup try 1

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 'use strict';
 
+import './sass/main.scss';
+
 import loadHome from './home';
 import {
   content,
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,58 @@
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const mode =
+  process.env.NODE_ENV === 'production' ? 'production' : 'development';
 
 module.exports = {
+  mode: mode,
+
+  plugins: [new MiniCssExtractPlugin()],
+
   entry: './src/index.js',
-  devtool: 'inline-source-map',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
+    // clean: true,
   },
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_mudules/,
+        use: {
+          // Without additional settings this will reference .babelrc
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.(s[ac]|c)ss$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'postcss-loader',
+          'less-loader',
+          'sass-loader',
+        ],
+      },
+    ],
+  },
+
+  module: {
+    rules: [
+      // шрифты и SVG
+      {
+        test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
+        type: 'asset/inline',
+      },
+    ],
+  },
+
+  devtool: 'source-map',
 };
